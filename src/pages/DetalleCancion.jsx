6fba21321a2b00ab01f4aa37220bd091393cb524
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-21 14:48:24 -0300

    cambio lista por tabla de canciones sugeridas. instalo dependencia de bootstrap-icons

--- src/pages/DetalleCancion.jsx
+++ src/pages/DetalleCancion.jsx
@@ -1,21 +1,23 @@
 
 import Image from 'react-bootstrap/Image';
-import imgPortada from '../assets/imgDetalleCancion/Coldplay.jpg'
-// import imgPortada from '../assets/imgDetalleCancion/portadaYellow.jfif'
-import ListGroup from 'react-bootstrap/ListGroup';
-
+import imgPortada from '../assets/imgDetalleCancion/portadaYellow.jfif'
+import imgGrupo from '../assets/imgDetalleCancion/grupoColdplay.jfif'
+import imgCancion from '../assets/imgDetalleCancion/Coldplay.jpg'
+import imgAlbum from '../assets/imgDetalleCancion/vivaLaVida.png'
+import { Table, Button } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 const DetalleCancion = ({ arrayDetalleCancion }) => {
     return (
         <main className='fondoDetalleCancion'>
-            <section className='imagen-container position-relative'>
+            <section className='text-light position-relative'>
                 {/* portada de la cancion */}
                 <Image src={imgPortada} className='imgPortadaCancion' fluid alt='imagen portada canción o album' />
 
                 {/*Overlay Superior Izquierda: "Artista Verificado" */}
                 <div className="overlay top-left">
                     {/* Clase de Bootstrap para el badge (fondo y forma) */}
-                    <span className="badge bg-primary fs-2">Canción</span>
+                    <span className="badge bg-primary">Canción</span>
                 </div>
 
                 {/* Overlay centro: nombre Cancion */}
@@ -24,19 +26,59 @@ const DetalleCancion = ({ arrayDetalleCancion }) => {
                 </div>
 
                 {/* Overlay Inferior Izquierda: Detalles */}
-                {/* p-2: padding de Bootstrap, text-white: color de texto de Bootstrap */}
                 <div className="overlay bottom-left text-white p-2">
-                    
                     <p className="mb-0"><span className='fw-bold'>ColdPlay - Parachutes</span> - 2000 - 4:26 - 3.421.608.407</p>
                 </div>
             </section>
 
-            <section className='container imagen-container position-relative my-5'>
-                <ListGroup as="ul">
-                    <ListGroup.Item as="li" >Cras justo odio</ListGroup.Item>
-                    <ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
-                    <ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
-                </ListGroup>
+            <section className='container my-3'>
+                <Button className='bg-success'>
+                    play ▶️
+                    {/* <i className="bi bi-play-fill"></i> */}
+                </Button>
+            </section>
+
+            <section className='container d-flex align-items-center'>
+                {/* Imagen Circular */}
+                <Image src={imgGrupo} roundedCircle  alt='imagen del grupo' />
+
+                {/* Contenedor para los textos, colocado junto a la imagen */}
+                <div className='ms-3'>
+                    <div className='fw-bold fs-3 text-light'>Artista</div>
+                    <Link className='fw-bold fs-3 text-light'>ColdPlay</Link>
+                </div>
+            </section>
+
+            <section className='px-3 py-2 mt-5 fondoTransparente'>
+                <h3>Recomendaciones</h3>
+                <p>Basadas en esta canción</p>
+            </section>
+
+            <section className='px-3 mb-5 table-responsive'>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr className='text-center'>
+                            <th>#</th>
+                            {/* <th>link album</th> */}
+                            <th>Album</th>
+                            <th>Titulo</th>
+                            <th>Categoría</th>
+                            <th>Duración</th>
+                            <th>▶️</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            {/* <td><Image src={imgAlbum} thumbnail  alt='imagen de la cancion'/></td> */}
+                            <td>Viva La Vida or Death and All His Friends</td>
+                            <td>Viva La Vida</td>
+                            <td>Pop</td>
+                            <td>4:02</td>
+                            <td>link cancion</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </section>
         </main>
 
