bee0e9b8d82710413bc5c38d25af1c94c3247582
jonathan <jonathanfiorenza6@gmail.com>
2025-11-20 22:05:21 -0300

    agregamos la funcion para el login para poder verificar email y contrasena y lo manda al localstorage

--- src/contenido/AuthContext.jsx
+++ src/contenido/AuthContext.jsx
@@ -8,3 +8,31 @@ export function AuthProvider({ children }) {
     const saved = localStorage.getItem("currentUser");
     if (saved) setUser(JSON.parse(saved));
   }, []);
+   useEffect(() => {
+    if (user) {
+      localStorage.setItem("currentUser", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("currentUser");
+    }
+  }, [user]);
+
+  const registerUser = (nombre, email, password) => {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    if (users.some((u) => u.email === email)) return false;
+
+    const newUser = { nombre, email, password, role: "user" };
+    users.push(newUser);
+
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUser", JSON.stringify(newUser));
+    setUser(newUser);
+
+    return true;
+  };
+const login = (email, password) => {
+    if (email === "admin@local.com" && password === "admin123") {
+      const admin = { email, role: "admin" };
+      setUser(admin);
+      return true;
+    }
