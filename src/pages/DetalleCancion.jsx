54a057285ec9096d39e895a0853bc055c2eb40a6
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-21 11:53:38 -0300

    cambio datos imagen primera imagen para que muestre etalles de la cancion

--- src/pages/DetalleCancion.jsx
+++ src/pages/DetalleCancion.jsx
@@ -14,22 +14,25 @@ const DetalleCancion = ({ arrayDetalleCancion }) => {
                 {/*Overlay Superior Izquierda: "Artista Verificado" */}
                 <div className="overlay top-left">
                     {/* Clase de Bootstrap para el badge (fondo y forma) */}
-                    <span className="badge bg-success">✅ Artista Verificado</span>
+                    <span className="badge bg-success">Canción</span>
+                </div>
+
+                {/* Overlay centro: nombre Cancion */}
+                <div className="overlay centered-text text-white p-2">
+                    <h1 className="mb-0 fw-bold display-1">YELLOW</h1>
                 </div>
 
                 {/* Overlay Inferior Izquierda: Detalles */}
                 {/* p-2: padding de Bootstrap, text-white: color de texto de Bootstrap */}
                 <div className="overlay bottom-left text-white p-2">
-                    <h5 className="mb-0">Nombre del Artista</h5>
-                    <p className="mb-0">1.250.000 Reproducciones</p>
+                    
+                    <p className="mb-0 fs-3 "><span className='fw-bold'>ColdPlay - Parachutes</span> - 2000 - 4:26 - 3.421.608.407</p>
                 </div>
             </section>
 
             <section className='container imagen-container position-relative my-5'>
                 <ListGroup as="ul">
-                    <ListGroup.Item as="li" >
-                        Cras justo odio
-                    </ListGroup.Item>
+                    <ListGroup.Item as="li" >Cras justo odio</ListGroup.Item>
                     <ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
                     <ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
                 </ListGroup>
