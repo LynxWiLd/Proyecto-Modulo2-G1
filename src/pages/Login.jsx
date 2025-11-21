3a22bd87971d0e39b3e71c840375da775d14c179
jonathan <jonathanfiorenza6@gmail.com>
2025-11-20 21:16:40 -0300

    agregue las importaciones y la configuracion inicial del login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,29 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { Form, Button, Card, Container } from "react-bootstrap";
+import Swal from "sweetalert2";
+import { useAuth } from "../contenido/AuthContext";
+import "../styles/stylologin.css";
+
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+   const{
+register,
+ handleSubmit,
+  formState: { errors }
+} = useForm();
+const { login } = useAuth();
+const navigate = useNavigate();
+
+
+
+
+
+
+
+
+}
+   
+   
+
 
 export default Login;
