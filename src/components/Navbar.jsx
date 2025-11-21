5e7b23668ffaa692b9a8b39706a70951b5e7c56e
jonathan <jonathanfiorenza6@gmail.com>
2025-11-20 22:45:45 -0300

    se agrego la logica del boton en el navbar para cuando estemos dentro de la sesion nos aparezca tambien para cerrarla

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,45 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Icon from "../assets/icon.png";
+import { useAuth } from "../contenido/AuthContext";   
 
-const NavigationBar = () => (
-  <Navbar bg="dark" variant="dark" expand="lg" className="px-5">
-    <img src={Icon} alt="icono" id="icon" />
-    <Navbar.Brand as={Link} to="/">
-      Mi Sitio
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbar-nav" />
-    <Navbar.Collapse id="navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link as={Link} to="/">
-          Inicio
-        </Nav.Link>
-        <Nav.Link as={Link} to="/about">
-          Sobre Nosotros
-        </Nav.Link>
-      </Nav>
-      <Nav>
-        <Nav.Link as={Link} to="/login">
-          Iniciar Sesion
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+const NavigationBar = () => {
+  const { user, logout } = useAuth();   
+
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className="px-5">
+      <img src={Icon} alt="icono" id="icon" />
+      
+      <Navbar.Brand as={Link} to="/">
+        Mi Sitio
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">
+            Inicio
+          </Nav.Link>
+
+          <Nav.Link as={Link} to="/about">
+            Sobre Nosotros
+          </Nav.Link>
+        </Nav>
+
+        <Nav>
+          {user ? (
+            <Nav.Link onClick={logout}>
+              Cerrar sesión
+            </Nav.Link>
+          ) : (
+            <Nav.Link as={Link} to="/login">
+              Iniciar Sesion
+            </Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default NavigationBar;
