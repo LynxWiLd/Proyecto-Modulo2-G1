85755ec093ff8992e7119eafdd3ec42c048d5348
jonathan <jonathanfiorenza6@gmail.com>
2025-11-20 22:02:32 -0300

    cargamos sesion desde local storage

--- src/contenido/AuthContext.jsx
+++ src/contenido/AuthContext.jsx
@@ -2,4 +2,9 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
-
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const saved = localStorage.getItem("currentUser");
+    if (saved) setUser(JSON.parse(saved));
+  }, []);
