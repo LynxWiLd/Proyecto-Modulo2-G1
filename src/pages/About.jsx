0cbd1b429bcaedb707d67f516a2af5b1a33c1e29
Corina <corinajimenez061102@gmail.com>
2025-11-21 22:26:12 -0300

    agregue grilla con los miembros del equipo con estilos y un efecto de agrandamiento de car

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,40 @@
 import { Container } from "react-bootstrap";
 
+const integrantes = [
+  {
+    nombre: "Integrante 1",
+    rol: "Frontend Developer",
+    img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+    
+  },
+  {
+    nombre: "Integrante 2",
+    rol: "Backend Developer",
+    img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+  },
+  {
+    nombre: "Integrante 3",
+    rol: "UX/UI Designer",
+    img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+  },
+  {
+    nombre: "Integrante 4",
+    rol: "Project Manager",
+    img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+  },
+  {
+    nombre: "Integrante 5",
+    rol: "QA Tester",
+    img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+  },
+  {
+    nombre: "Integrante 6",
+    rol: "Full Stack Developer",
+    img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+  }
+];
 const About = () => (
+  
   <Container className="my-5">
     <div className="row g-4 align-items-center">
 
@@ -24,6 +58,29 @@ const About = () => (
       </div>
 
     </div>
+    
+      <section className="my-5 section-about-team">
+        <h3 className="fs-2 text-center mb-4">Nuestro Equipo</h3>
+<hr />
+        <div className="row g-4">
+          {integrantes.map((persona, index) => (
+            <div className="col-12 col-md-6 col-lg-4 about-card" key={index}>
+              <div className="card shadow-sm h-100 border-0">
+                <img
+                  src={persona.img}
+                  className="card-img-top"
+                  alt={persona.nombre}
+                 
+                />
+                <div className="card-body">
+                  <h5 className="card-title fw-bold">{persona.nombre}</h5>
+                  <h6 className="text-primary">{persona.rol}</h6>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
   </Container>
 );
 
