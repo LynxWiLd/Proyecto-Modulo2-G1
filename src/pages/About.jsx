07fbc2666fd983f55724098a7203a3e5847c8105
Corina <corinajimenez061102@gmail.com>
2025-11-22 17:33:39 -0300

    agregar boton que direccione al home

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,11 @@
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const integrantes = [
   {
     nombre: "Integrante 1",
     rol: "Frontend Developer",
     img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
-    
   },
   {
     nombre: "Integrante 2",
@@ -33,35 +33,39 @@ const integrantes = [
     img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
   }
 ];
-const About = () => (
-  
-  <Container className="my-5">
-    <div className="row g-4 align-items-center">
 
-      {/* Texto a la izquierda */}
-      <div className="col-lg-6 col-md-6 col-sm-12 div-about-encabezado py-4">
-        <h1 className="fw-bold">Sobre Nosotros</h1>
-        <p className="fs-5">
-          Somos un equipo de desarrollo web trabajando en una plataforma musical
-          moderna, inspirada en experiencias como Spotify y YouTube Music.
-        </p>
+const About = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container className="my-5">
+
+      <div className="row g-4 align-items-center">
+
+        {/* Texto a la izquierda */}
+        <div className="col-lg-6 col-md-6 col-sm-12 div-about-encabezado py-4">
+          <h1 className="fw-bold">Sobre Nosotros</h1>
+          <p className="fs-5">
+            Somos un equipo de desarrollo web trabajando en una plataforma musical
+            moderna, inspirada en experiencias como Spotify y YouTube Music.
+          </p>
+        </div>
+
+        {/* Imagen a la derecha */}
+        <div className="col-lg-6 col-md-6 col-sm-12 text-center">
+          <img
+            src="https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg"
+            alt="Equipo"
+            className="img-fluid rounded object-fit-cover img-about"
+          />
+        </div>
+
       </div>
 
-      {/* Imagen a la derecha */}
-      <div className="col-lg-6 col-md-6 col-sm-12 text-center ">
-        <img
-          src="https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg"
-          alt="Equipo"
-          className="img-fluid rounded object-fit-cover img-about"
-          
-        />
-      </div>
-
-    </div>
-    
       <section className="section-about-team">
         <h3 className="fs-2 text-center mb-4">Nuestro Equipo</h3>
-<hr/>
+        <hr />
+
         <div className="row g-4">
           {integrantes.map((persona, index) => (
             <div className="col-12 col-md-6 col-lg-4 about-card" key={index}>
@@ -70,7 +74,6 @@ const About = () => (
                   src={persona.img}
                   className="card-img-top"
                   alt={persona.nombre}
-                 
                 />
                 <div className="card-body">
                   <h5 className="card-title fw-bold">{persona.nombre}</h5>
@@ -81,7 +84,16 @@ const About = () => (
           ))}
         </div>
       </section>
-  </Container>
-);
+
+     
+      <div className="text-center my-4">
+        <Button variant="success" onClick={() => navigate("/")}>
+          Conoce más sobre este proyecto!
+        </Button>
+      </div>
+
+    </Container>
+  );
+};
 
 export default About;
