66c4122fbbd0dd9d6a22e082d815356a63802129
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-23 09:08:39 -0300

    instalo bootstrap-icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.css";
 
