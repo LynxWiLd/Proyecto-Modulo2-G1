e05ecca8327b37ed2dfce1ee2950dccb8b6112f7
Vera_Facundo <facundovera647@gmail.com>
2025-12-04 08:56:08 -0300

    mejora en la grilla de formulario

--- src/components/shared/FormularioCancion.jsx
+++ src/components/shared/FormularioCancion.jsx
@@ -1,4 +1,4 @@
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
@@ -76,123 +76,242 @@ const FormularioCancion = ({
       <hr />
 
       <Form onSubmit={handleSubmit(onSubmit)}>
-        {/* Nombre Canción */}
-        <Form.Group className="mb-3">
-          <Form.Label>Título de la canción</Form.Label>
-          <Form.Control
-            type="text"
-            {...register("nombreCancion", {
-              required: "El título es obligatorio",
-            })}
-          />
-          <Form.Text className="text-danger">
-            {errors.nombreCancion?.message}
-          </Form.Text>
-        </Form.Group>
+        {/* Fila 1: Artista/Grupo */}
+        <Row>
+          <Col xs={12}>
+            <Form.Group
+              controlId="exampleForm.ControlArtistaGrupo"
+              className="text-dark mb-3"
+            >
+              <Form.Label>Artista/Grupo:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Nombre del Artista o Grupo"
+                {...register("artistaGrupo", {
+                  required: "El artista o Grupo es un dato obligatorio",
+                  minLength: {
+                    value: 2,
+                    message:
+                      "El artista o Grupo debe contener como mínimo 2 caracteres",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message:
+                      "El artista debe contener como máximo 50 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.artistaGrupo?.message}
+              </Form.Text>
+            </Form.Group>
+          </Col>
+        </Row>
 
-        {/* Artista */}
-        <Form.Group className="mb-3">
-          <Form.Label>Artista / Grupo</Form.Label>
-          <Form.Control
-            type="text"
-            {...register("artistaGrupo", {
-              required: "El artista es obligatorio",
-            })}
-          />
-          <Form.Text className="text-danger">
-            {errors.artistaGrupo?.message}
-          </Form.Text>
-        </Form.Group>
+        {/* Fila 2: Título Canción y Album */}
+        <Row>
+          <Col xs={12} md={6} lg={6}>
+            <Form.Group
+              controlId="exampleForm.ControlTituloCancion"
+              className="text-dark mb-3"
+            >
+              <Form.Label>Título Canción:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Nombre de la canción"
+                {...register("nombreCancion", {
+                  required: "El nombre de la canción es un dato obligatorio",
+                  minLength: {
+                    value: 2,
+                    message:
+                      "El nombre de la canción debe contener como mínimo 2 caracteres",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message:
+                      "El nombre de la canción debe contener como máximo 50 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.nombreCancion?.message}
+              </Form.Text>
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={6} lg={6}>
+            <Form.Group
+              controlId="exampleForm.ControlAlbumCancion"
+              className="text-dark mb-3"
+            >
+              <Form.Label>Album:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Nombre del Album"
+                {...register("nombreAlbum", {
+                  required: "El nombre del Album es un dato obligatorio",
+                  minLength: {
+                    value: 2,
+                    message:
+                      "El nombre del Album debe contener como mínimo 2 caracteres",
+                  },
+                  maxLength: {
+                    value: 30,
+                    message:
+                      "El nombre del Album debe contener como máximo 30 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.nombreAlbum?.message}
+              </Form.Text>
+            </Form.Group>
+          </Col>
+        </Row>
 
-        {/* Álbum */}
-        <Form.Group className="mb-3">
-          <Form.Label>Álbum</Form.Label>
-          <Form.Control
-            type="text"
-            {...register("albumCancion", {
-              required: "El álbum es obligatorio",
-            })}
-          />
-          <Form.Text className="text-danger">
-            {errors.albumCancion?.message}
-          </Form.Text>
-        </Form.Group>
+        {/* Fila 3: Año Canción y Categoría (Select) y duracion */}
+        <Row>
+          <Col xs={12} md={4} lg={4}>
+            <Form.Group
+              controlId="exampleForm.ControlAnioCancion"
+              className="text-dark mb-3"
+            >
+              <Form.Label>Año Canción:</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Año Canción"
+                {...register("anioCancion", {
+                  required: "El año de la canción es un dato obligatorio",
+                  min: {
+                    value: 1900,
+                    message:
+                      "El año de la canción debe ser igual o mayor a 1900",
+                  },
+                  max: {
+                    value: 2025,
+                    message:
+                      "El año de la canción debe ser igual o mayor a 2025",
+                  },
+                  pattern: {
+                    value: /^\d{4}$/, // Asegura que sean 4 dígitos exactos
+                    message:
+                      "Por favor, ingresa un año válido (cuatro dígitos).",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.anioCancion?.message}
+              </Form.Text>
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={4} lg={4}>
+            <Form.Group
+              controlId="exampleForm.ControlCategoriaCancion"
+              className="text-dark mb-3"
+            >
+              <Form.Label>Categoría:</Form.Label>
+              <Form.Select
+                aria-label="Seleccionar Categoría"
+                {...register("categoriaCancion", {
+                  required: "Debe seleccionar una Categoría",
+                })}
+              >
+                <option value="">Seleccione uns Categoría</option>
+                <option value="Rock">Rock</option>
+                <option value="Pop">Pop</option>
+                <option value="Electronica">Electrónica</option>
+                <option value="Cuarteto">Cuarteto</option>
+                <option value="Rap">Rap</option>
+                <option value="Folclore">Folclore</option>
+                <option value="Reggae">Reggae</option>
+                <option value="Clasica">Clásica</option>
+                <option value="Tango">Tango</option>
+                <option value="Jazz">Jazz</option>
+                <option value="Blues">Blues</option>
+                <option value="Country">Country</option>
+              </Form.Select>
+              <Form.Text className="text-danger">
+                {errors.categoriaCancion?.message}
+              </Form.Text>
+            </Form.Group>
+          </Col>
 
-        {/* Año */}
-        <Form.Group className="mb-3">
-          <Form.Label>Año</Form.Label>
-          <Form.Control
-            type="number"
-            {...register("anioCancion", {
-              required: "El año es obligatorio",
-              min: { value: 1900, message: "Año inválido" },
-              max: { value: new Date().getFullYear(), message: "Año inválido" },
-            })}
-          />
-          <Form.Text className="text-danger">
-            {errors.anioCancion?.message}
-          </Form.Text>
-        </Form.Group>
+          <Col xs={12} md={4} lg={4}>
+            <Form.Group
+              controlId="exampleForm.ControlduracionCancion"
+              className="text-dark mb-3"
+            >
+              <Form.Label>Duración Canción:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="MM:SS"
+                {...register("duracionCancion", {
+                  required: "La duración de la canción es un dato obligatorio",
+                  minLength: {
+                    value: 1,
+                    message: "La duración de la canción como mínimo 1 caracter",
+                  },
+                  maxLength: {
+                    value: 5,
+                    message:
+                      "La duración de la canción debe contener como máximo 5 caracteres, incluyendo los :",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.duracionCancion?.message}
+              </Form.Text>
+            </Form.Group>
+          </Col>
+        </Row>
 
-        {/* Categoría */}
-        <Form.Group className="mb-3">
-          <Form.Label>Categoría</Form.Label>
-          <Form.Control
-            type="text"
-            {...register("categoriaCancion", {
-              required: "La categoría es obligatoria",
-            })}
-          />
-          <Form.Text className="text-danger">
-            {errors.categoriaCancion?.message}
-          </Form.Text>
-        </Form.Group>
+        {/* Fila 5: URL Portada y URL Canción */}
+        <Row>
+          <Col xs={12}>
+            <Form.Group
+              controlId="exampleForm.ControlUrlImgCancion"
+              className="text-dark mb-3"
+            >
+              <Form.Label>URL Imágen portada:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="archivo de imagen de portada de la canción"
+                {...register("urlImgCancion", {
+                  required:
+                    "La URL de la imágen de portada de la canción es un dato obligatorio",
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.urlImgCancion?.message}
+              </Form.Text>
+            </Form.Group>
+          </Col>
 
-        {/* Duración */}
-        <Form.Group className="mb-3">
-          <Form.Label>Duración (ej: 3:45)</Form.Label>
-          <Form.Control
-            type="text"
-            {...register("duracionCancion", {
-              required: "La duración es obligatoria",
-            })}
-          />
-          <Form.Text className="text-danger">
-            {errors.duracionCancion?.message}
-          </Form.Text>
-        </Form.Group>
+          <Col xs={12}>
+            <Form.Group
+              controlId="exampleForm.ControlUrlCancion"
+              className="text-dark mb-3"
+            >
+              <Form.Label>URL:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="archivo para escuchar la canción"
+                {...register("urlCancion", {
+                  required: "La URL de la canción es un dato obligatorio",
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.urlCancion?.message}
+              </Form.Text>
+            </Form.Group>
+          </Col>
+        </Row>
 
-        {/* URL canción */}
-        <Form.Group className="mb-3">
-          <Form.Label>URL de la canción</Form.Label>
-          <Form.Control
-            type="text"
-            {...register("urlCancion", {
-              required: "La URL es obligatoria",
-            })}
-          />
-          <Form.Text className="text-danger">
-            {errors.urlCancion?.message}
-          </Form.Text>
-        </Form.Group>
-
-        {/* Imagen */}
-        <Form.Group className="mb-3">
-          <Form.Label>URL de la imagen</Form.Label>
-          <Form.Control
-            type="text"
-            {...register("urlImgCancion", {
-              required: "La imagen es obligatoria",
-            })}
-          />
-          <Form.Text className="text-danger">
-            {errors.urlImgCancion?.message}
-          </Form.Text>
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Guardar
-        </Button>
+        <div className="text-center py-3">
+          <Button variant="success px-5" type="submit">
+            {" "}
+            Enviar
+          </Button>
+        </div>
       </Form>
     </Container>
   );
